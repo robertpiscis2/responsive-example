c1611675951cf271655b548cf657742f2be52cb9
Roberto Ramirez <42901082+robertpiscis2@users.noreply.github.com>
2020-04-30 00:28:00 -0500

    deploy1.0

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flexbox Proyecto 1</title>
+    <link rel="stylesheet" href="css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lora&family=Lato&family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <div class="main">
+        <header class="header">
+            <div class="header-menu">
+                <div class="menu-logo">
+                    <a href="index.html">Genial v.1</a>
+                </div>
+                <nav class="menu-nav">
+                    <a href="#"><i class="fas fa-home"></i><span class="off">Inicio</span></a>
+                    <a href="#"><i class="fas fa-user"></i><span class="off">Nosotros</span></a>
+                    <a href="#"><i class="fas fa-comments"></i><span class="off">Servicios</span></a>
+                    <a href="#"><i class="fas fa-file"></i><span class="off">Noticias</span></a>
+                    <a href="#"><i class="fas fa-envelope"></i><span class="off">Contacto</span></a>
+                </nav>
+                <div class="menu-social">
+                    <div>
+                        <a href="#"><i class="fab fa-facebook-square"></i></a>
+                    </div>
+                    <div>
+                        <a href="#"><i class="fab fa-twitter-square"></i></a>
+                    </div>
+                    <div>
+                        <a href="#"><i class="fab fa-instagram-square"></i></a>
+                    </div>
+                    <div>
+                        <a href="#"><i class="fab fa-youtube-square"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="header-texto">
+                <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam modi ipsa impedit eaque praesentium voluptatem nesciunt laudantium, odio itaque earum molestias numquam esse, porro placeat incidunt nam ex culpa eum voluptatum nostrum expedita! Provident labore optio corrupti impedit consequatur vitae aspernatur laudantium iste, minus nemo doloremque iusto fugiat facilis exercitationem?</h1>
+            </div> 
+        </header>
+
+        <section class="container">
+            <div class="articles">
+                <article class="cont-article">
+                    <img src="images/2.jpg" alt="">
+                    <h3>29 de abril de 2020</h3>
+                    <h2>Titulo de la noticia</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sed at assumenda suscipit magni, id nisi repudiandae. Accusamus aperiam officia ea debitis itaque, nihil ad iusto nemo facilis quos velit.</p>
+                    <a href="#">Read more...</a>
+                </article>
+                <article class="cont-article">
+                    <img src="images/2.jpg" alt="">
+                    <h3>29 de abril de 2020</h3>
+                    <h2>Titulo de la noticia</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sed at assumenda suscipit magni, id nisi repudiandae. Accusamus aperiam officia ea debitis itaque, nihil ad iusto nemo facilis quos velit.</p>
+                    <a href="#">Read more...</a>
+                </article>
+                <article class="cont-article">
+                    <img src="images/2.jpg" alt="">
+                    <h3>29 de abril de 2020</h3>
+                    <h2>Titulo de la noticia</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sed at assumenda suscipit magni, id nisi repudiandae. Accusamus aperiam officia ea debitis itaque, nihil ad iusto nemo facilis quos velit.</p>
+                    <a href="#">Read more...</a>
+                </article>
+                <article class="cont-article">
+                    <img src="images/2.jpg" alt="">
+                    <h3>29 de abril de 2020</h3>
+                    <h2>Titulo de la noticia</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sed at assumenda suscipit magni, id nisi repudiandae. Accusamus aperiam officia ea debitis itaque, nihil ad iusto nemo facilis quos velit.</p>
+                    <a href="#">Read more...</a>
+                </article>
+                <nav class="cont-nav">
+                    <a href="#">Inicio</a>
+                    <a href="#">1</a>
+                    <a href="#">2</a>
+                    <a href="#">3</a>
+                    <a href="#">4</a>
+                    <a href="#">Final</a>
+                </nav>
+            </div>
+            <aside class="aside-right">
+                <div class="aside-item">
+                    <h4>Categoria</h4>
+                    <img src="images/2.jpg" alt="">
+                </div>
+                <div class="aside-item">
+                    <h4>Categoria</h4>
+                    <img src="images/2.jpg" alt="">
+                </div>
+                <div class="aside-item">
+                    <h4>Categoria</h4>
+                    <img src="images/2.jpg" alt="">
+                </div>
+            </aside>
+        </section>
+
+        <footer class="footer">
+            <nav class="footer-nav">
+                <a href="#">Desarrollado por Beto Ramirez</a>
+            </nav>
+        </footer>
+    </div>
+</body>
+</html>
